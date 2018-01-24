426aee4b0dbe2a601b7029fcfaf169af552d32d0
Josselin <josselin.lefevre@edu.esiee.fr>
2018-01-24 23:54:47 +0100

    deuxieme seance

---
+++ TP1/_4.3/README.txt
@@ -0,0 +1,3 @@
+Vs = Ve*((1/jcw)/(R+(1/jcw)))=Ve*(1/(1+jRcw))~~=Ve=1/1+(f/fc))
+
+=> fc=1/(2*piRC)
--- TP1/_4.3/_4.3.ino
+++ TP1/_4.3/_4.3.ino
@@ -1,30 +1,54 @@
 #define led1 11
+int i = 0;
+float sommeTmp = 0;
 
 // the setup routine runs once when you press reset:
-void setup() {
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
-  pinMode(led1, OUTPUT);
+void setup()
+{
+    // initialize serial communication at 9600 bits per second:
+    Serial.begin(9600);
+    pinMode(led1, OUTPUT);
 }
 
 // the loop routine runs over and over again forever:
-void loop() {
+void loop()
+{
+
+  if(i==3){
+    i+0;
+    Serial.print("Temperature moyenne : ");
+    Serial.println(sommeTmp/3);
+    sommeTmp=0;
+    i=0;
+    }
+  else{
+    i++;
+    sommeTmp+=getTmp();
+    }
   
-  // read the input on analog pin 0:
-  int sensorValue = analogRead(A0); // converting that reading to voltage, for 3.3v arduino use 3.3
- float voltage = sensorValue * 5.0;
- voltage /= 1024.0; 
- 
- // print out the voltage
- Serial.print(voltage); Serial.println(" volts");
- 
- // now print out the temperature
- float temperatureC = (voltage - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset
-                                               //to degrees ((voltage - 500mV) times 100)
- Serial.print(temperatureC); Serial.println(" degrees C");
-  // print out the value you read:
-  int intensite = map (sensorValue,0,1023,0,255);
-  //analogWrite(led1, intensite);
-  Serial.println(intensite);
+  
   delay(1000);        // delay in between reads for stability
+}
+
+float getTmp()
+{
+    // read the input on analog pin 0:
+    int sensorValue = analogRead(A0); // converting that reading to voltage, for 3.3v arduino use 3.3
+    Serial.print(sensorValue);
+    Serial.println(" sensorValue");
+    float voltage = sensorValue * 5.0;
+    voltage = voltage *0.1+ 0.64;
+    voltage /= 1024.0;
+
+    // print out the voltage
+    Serial.print(voltage);
+    Serial.println(" volts");
+
+   
+    float temperatureC = (voltage - 0.5) * 100; 
+   
+    Serial.print(temperatureC);
+    Serial.println(" degrees C");
+    // print out the value you read:
+    return temperatureC;
 }
