97b0086aef09c3742a7bfb205ad97d532e4f851d
Victor Thak <35684605+VictorThak@users.noreply.github.com>
2018-05-13 23:22:22 +0200

    Create index7.js

---
+++ back-end/functions/index7.js
@@ -0,0 +1,76 @@
+
+var firebase = require("firebase");
+
+var config = {
+  apiKey: "AIzaSyCgu8NlOair0fbSJwuSOvzk9uZ1pn2tRdI",
+  authDomain: "test-back-8653e.firebaseapp.com",
+  databaseURL: "https://test-back-8653e.firebaseio.com/",
+};
+
+firebase.initializeApp(config);
+
+// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+const functions = require('firebase-functions');
+
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+exports.addMessage = functions.https.onRequest((req, res) => {
+  const original = req.query.text;
+  return admin.database().ref('/messages').push({original: original}).then((snapshot) => {
+    return res.redirect(303, snapshot.ref);
+  });
+  console.log("heyyyyyyyyyyyyyyyyyyyy");
+});
+
+
+exports.makeUppercase = functions.database.ref('/messages/{pushId}/original').onWrite((event) => {
+  const original = event.data.val();
+  console.log('Uppercasing', event.params.pushId, original);
+  const uppercase = original.toUpperCase();
+  return event.data.ref.parent.child('uppercase').set(uppercase);
+});
+
+exports.test = functions.database.ref('/users/jojo/score').onWrite((event) => {
+	console.log("onWrite listener OK");
+
+	//const score = event.data.val();
+	//console.log("Score is : ",score);
+	var tab0 = [];
+	var tab1 = [];
+	var l = firebase.database().ref("users").length;
+	console.log("length : " , l);
+	var HS = firebase.database().ref("users");
+	HS.orderByChild("score").once("value", function(snapshot) {console.log(snapshot.key + " : " + snapshot.val().score);});
+	firebase.database().ref("users").once("value").then(function(snapshot) {
+    	snapshot.forEach(function(childSnapshot) {	
+      	var key = childSnapshot.key;
+      	var childData = childSnapshot.val();
+		
+		tab0.push(key);
+		tab1.push(childSnapshot.child("score").val());
+      	console.log("Donnees de ",key," : ",childData);
+		var score = childSnapshot.child("score").val();
+		console.log("Score : ",score);
+  		});
+		
+
+
+	});
+});
+
+function tabMax(tab){
+	var max;
+	var v;
+	for (i=0; i<tab.length; i++) {
+		if (i == 0) {
+			v = 0;
+		}
+		else {
+			if (tab[i] > tab[v]){
+				v = i;
+			}
+		}
+	}
+	return v;
+}
